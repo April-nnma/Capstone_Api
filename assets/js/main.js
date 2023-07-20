3ed2aa899e791e0d9c978ae36a56df04db985a1a
Nguyen Anh <nguyenanh1904.vn@gmail.com>
2023-07-20 20:20:31 +0700

    done

--- assets/js/main.js
+++ assets/js/main.js
@@ -16,16 +16,49 @@
 
   // Sticky Navbar
   $(window).scroll(function () {
-    if ($(this).scrollTop() > 300) {
-      $(".sticky-top").addClass("shadow-sm").css("top", "0px");
+    if ($(this).scrollTop() > 45) {
+      $(".navbar").addClass("sticky-top shadow-sm");
     } else {
-      $(".sticky-top").removeClass("shadow-sm").css("top", "-150px");
+      $(".navbar").removeClass("sticky-top shadow-sm");
     }
+  });
+
+  // Dropdown on mouse hover
+  const $dropdown = $(".dropdown");
+  const $dropdownToggle = $(".dropdown-toggle");
+  const $dropdownMenu = $(".dropdown-menu");
+  const showClass = "show";
+
+  $(window).on("load resize", function () {
+    if (this.matchMedia("(min-width: 992px)").matches) {
+      $dropdown.hover(
+        function () {
+          const $this = $(this);
+          $this.addClass(showClass);
+          $this.find($dropdownToggle).attr("aria-expanded", "true");
+          $this.find($dropdownMenu).addClass(showClass);
+        },
+        function () {
+          const $this = $(this);
+          $this.removeClass(showClass);
+          $this.find($dropdownToggle).attr("aria-expanded", "false");
+          $this.find($dropdownMenu).removeClass(showClass);
+        }
+      );
+    } else {
+      $dropdown.off("mouseenter mouseleave");
+    }
+  });
+
+  // Facts counter
+  $('[data-toggle="counter-up"]').counterUp({
+    delay: 10,
+    time: 2000,
   });
 
   // Back to top button
   $(window).scroll(function () {
-    if ($(this).scrollTop() > 300) {
+    if ($(this).scrollTop() > 100) {
       $(".back-to-top").fadeIn("slow");
     } else {
       $(".back-to-top").fadeOut("slow");
@@ -36,35 +69,13 @@
     return false;
   });
 
-  // Modal Video
-  var $videoSrc;
-  $(".btn-play").click(function () {
-    $videoSrc = $(this).data("src");
-  });
-  console.log($videoSrc);
-  $("#videoModal").on("shown.bs.modal", function (e) {
-    $("#video").attr(
-      "src",
-      $videoSrc + "?autoplay=1&amp;modestbranding=1&amp;showinfo=0"
-    );
-  });
-  $("#videoModal").on("hide.bs.modal", function (e) {
-    $("#video").attr("src", $videoSrc);
-  });
-
-  // Product carousel
-  $(".product-carousel").owlCarousel({
+  // Testimonials carousel
+  $(".testimonial-carousel").owlCarousel({
     autoplay: true,
-    smartSpeed: 1000,
-    margin: 25,
+    smartSpeed: 1500,
+    dots: true,
     loop: true,
     center: true,
-    dots: false,
-    nav: true,
-    navText: [
-      '<i class="bi bi-chevron-left"></i>',
-      '<i class="bi bi-chevron-right"></i>',
-    ],
     responsive: {
       0: {
         items: 1,
@@ -81,13 +92,27 @@
     },
   });
 
-  // Testimonial carousel
-  $(".testimonial-carousel").owlCarousel({
+  // Vendor carousel
+  $(".vendor-carousel").owlCarousel({
+    loop: true,
+    margin: 45,
+    dots: false,
+    loop: true,
     autoplay: true,
     smartSpeed: 1000,
-    items: 1,
-    loop: true,
-    dots: true,
-    nav: false,
+    responsive: {
+      0: {
+        items: 2,
+      },
+      576: {
+        items: 4,
+      },
+      768: {
+        items: 6,
+      },
+      992: {
+        items: 8,
+      },
+    },
   });
 })(jQuery);
